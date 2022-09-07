e86fd67c7ffae2b044d586bd1dbc78a88f32a7e5
Anton Kozyriev <sonara4mus@gmail.com>
2022-09-08 02:12:20 +0300

    test(core): single variable smoothed gradient calculation

--- src/test/grad_test.py
+++ src/test/grad_test.py
@@ -13,6 +13,14 @@ from core.grad import *
 def non_smoothed_single_variable_params(request):
     return request.param
 
+@pytest.fixture(scope="function", params=[
+    (np.array([2.0]), 1e-6, 32.0, 5, 1),
+    (np.array([2.0]), 1e-6, 32.0, 5, 3),
+    (np.array([2.0]), 1e-6, 32.0, 5, 5),
+    (np.array([2.0]), 1e-6, 32.0, 5, 10),
+    (np.array([2.0]), 1e-6, 32.0, 5, 250)])
+def smoothed_single_variable_params(request):
+    return request.param
 
 def test_non_smoothed_should_calculate_grad_vector_single_variable(non_smoothed_single_variable_params):
     F = lambda x: x ** 3 + 2 * x ** 2 + 12 * x + 100
@@ -21,3 +29,11 @@ def test_non_smoothed_should_calculate_grad_vector_single_variable(non_smoothed_
     np.testing.assert_almost_equal(grad_left(F, x0, h=h), g0, decimal=dec)
     np.testing.assert_almost_equal(grad_center(F, x0, h=h), g0, decimal=dec)
     np.testing.assert_almost_equal(grad_right(F, x0, h=h), g0, decimal=dec)
+
+def test_smoothed_should_calculate_grad_vector_single_variable(smoothed_single_variable_params):
+    np.random.seed(0)
+
+    F = lambda x: x ** 3 + 2 * x ** 2 + 12 * x + 100
+    (x0, h, g0, dec, k) = smoothed_single_variable_params # input variable, grid step, expected gradient value, decimal accuracy, smoothing term
+
+    np.testing.assert_almost_equal(grad_smoothed(F, x0, h=h, k=k), g0, decimal=dec)
