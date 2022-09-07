c39b338746de50bc0ebc2fa7d2d4991da776e360
Anton Kozyriev <sonara4mus@gmail.com>
2022-09-08 02:29:52 +0300

    test(core): multiple variable non smoothed gradient calculation

--- src/test/grad_test.py
+++ src/test/grad_test.py
@@ -22,6 +22,15 @@ def non_smoothed_single_variable_params(request):
 def smoothed_single_variable_params(request):
     return request.param
 
+@pytest.fixture(scope="function", params=[
+    (np.array([2.0, -1.0]), 1e-6, np.array([3.0, 0.0]), 5),
+    (np.array([2.0, -1.0]), 0.01, np.array([3.0, 0.0]), 3),
+    (np.array([0.0, 0.0]), 1e-6, np.array([0.0, 0.0]), 5),
+    (np.array([0.0, 0.0]), 1e-7, np.array([0.0, 0.0]), 6),
+    (np.array([-5.0, 10.0]), 1e-6, np.array([0.0, 15.0]), 5)])
+def non_smoothed_multiple_variable_params(request):
+    return request.param
+
 def test_non_smoothed_should_calculate_grad_vector_single_variable(non_smoothed_single_variable_params):
     F = lambda x: x ** 3 + 2 * x ** 2 + 12 * x + 100
     (x0, h, g0, dec) = non_smoothed_single_variable_params # input variable, grid step, expected gradient value, decimal accuracy
@@ -37,3 +46,11 @@ def test_smoothed_should_calculate_grad_vector_single_variable(smoothed_single_v
     (x0, h, g0, dec, k) = smoothed_single_variable_params # input variable, grid step, expected gradient value, decimal accuracy, smoothing term
 
     np.testing.assert_almost_equal(grad_smoothed(F, x0, h=h, k=k), g0, decimal=dec)
+
+def test_non_smoothed_should_calculate_grad_vector_multiple_variable(non_smoothed_multiple_variable_params):
+    F = lambda x: x[0] ** 2 + x[0] * x[1] + x[1] ** 2
+    (x0, h, g0, dec) = non_smoothed_multiple_variable_params
+
+    np.testing.assert_almost_equal(grad_left(F, x0, h=h), g0, decimal=dec)
+    np.testing.assert_almost_equal(grad_center(F, x0, h=h), g0, decimal=dec)
+    np.testing.assert_almost_equal(grad_right(F, x0, h=h), g0, decimal=dec)
